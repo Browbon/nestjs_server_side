9cbf1b32e3438384712c254aeded4894b1475a8f
Browbon <quangpd98@gmail.com>
2024-05-24 20:13:32 +0700

    drizzle config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { DaoModule } from './dao/dao.module';
-import { UserDaoService } from './dao/userDao.service';
 
 @Module({
-  imports: [UsersModule, DaoModule],
+  imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService, UserDaoService],
+  providers: [AppService],
 })
 export class AppModule {}
